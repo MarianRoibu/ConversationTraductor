5ba327eaf79c4a8103d7736f60e2ba1071e4ef9d
Marian Roibu <marianroibu32@gmail.com>
2023-04-03 15:30:52 +0200

    add: speach to text function

--- src/pages/TranslateForm.jsx
+++ src/pages/TranslateForm.jsx
@@ -1,71 +1,77 @@
 import { useState } from 'react';
 import { translateText, detectLanguage } from '../functions/translationFunctions';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function TranslationInput() {
-  const [inputText, setInputText] = useState('');
-  const [translatedText, setTranslatedText] = useState('');
-  const [sourceLang, setSourceLang] = useState('');
-  const [targetLang, setTargetLang] = useState('es');
-
-  const handleInputChange = (event) => {
-    setInputText(event.target.value);
-  };
-
-  const handleSourceLangChange = (event) => {
-    setSourceLang(event.target.value);
-  };
-
-  const handleTargetLangChange = (event) => {
-    setTargetLang(event.target.value);
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const apiKey = 'afb08084-f865-93bd-398d-35465d9a2ad7:fx';
-    const { translatedText, detectedSourceLang } = await translateText(apiKey, sourceLang, targetLang, inputText);
-    setTranslatedText(translatedText);
-    setSourceLang(detectedSourceLang);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Enter text to translate:
-          <input type="text" value={inputText} onChange={handleInputChange} />
-        </label>
-        <label>
-          Source Language:
-          <select value={sourceLang} onChange={handleSourceLangChange}>
-            <option value="auto">Detect Language</option>
-            <option value="en">English</option>
-            <option value="es">Spanish</option>
-            <option value="fr">French</option>
-            <option value="de">German</option>
-            <option value="it">Italian</option>
-            <option value="ja">Japanese</option>
-          </select>
-        </label>
-        <label>
-          Target Language:
-          <select value={targetLang} onChange={handleTargetLangChange}>
-            <option value="en">English</option>
-            <option value="es">Spanish</option>
-            <option value="fr">French</option>
-            <option value="de">German</option>
-            <option value="it">Italian</option>
-            <option value="ja">Japanese</option>
-            <option value="ro">Roumanian</option>
-          </select>
-        </label>
-        <button type="submit">Translate</button>
-      </form>
-      {translatedText && <div>Translated text: {translatedText}</div>}
-    </div>
-  );
-}
-
-export default TranslationInput;
+    const [inputText, setInputText] = useState('');
+    const [translatedText, setTranslatedText] = useState('');
+    const [sourceLang, setSourceLang] = useState('');
+    const [targetLang, setTargetLang] = useState('es');
+  
+    const handleInputChange = (event) => {
+      setInputText(event.target.value);
+    };
+  
+    const handleSourceLangChange = (event) => {
+      setSourceLang(event.target.value);
+    };
+  
+    const handleTargetLangChange = (event) => {
+      setTargetLang(event.target.value);
+    };
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      const apiKey = 'afb08084-f865-93bd-398d-35465d9a2ad7:fx';
+      const { translatedText } = await translateText(apiKey, sourceLang, targetLang, inputText);
+      setTranslatedText(translatedText);
+    };
+  
+    const handleSpeechToText = () => {
+        const recognition = new window.webkitSpeechRecognition();
+        const langCode = sourceLang === 'auto' ? '' : sourceLang;
+      
+        recognition.onresult = (event) => {
+          setInputText(event.results[0][0].transcript);
+        };
+      
+        recognition.lang = langCode;
+        recognition.start();
+      };
+  
+    return (
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            Enter text to translate:
+            <input type="text" value={inputText} onChange={handleInputChange} />
+          </label>
+          <label>
+            Select source language:
+            <select value={sourceLang} onChange={handleSourceLangChange}>
+              <option value="auto">Detect language</option>
+              <option value="en">English</option>
+              <option value="es">Spanish</option>
+              <option value="ja">Japanese</option>
+            </select>
+          </label>
+          <label>
+            Select target language:
+            <select value={targetLang} onChange={handleTargetLangChange}>
+              <option value="es">Spanish</option>
+              <option value="en">English</option>
+              <option value="ja">Japanese</option>
+            </select>
+          </label>
+          <button type="submit">Translate</button>
+          <button type="button" onClick={handleSpeechToText}>Speak</button>
+        </form>
+        {translatedText && <div>Translated text: {translatedText}</div>}
+      </div>
+    );
+  }
+  
+  export default TranslationInput;
 
 
 
